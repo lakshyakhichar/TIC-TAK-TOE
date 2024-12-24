ebcfc858f3a21821d8e04a53ba6d659c1ffb2a31
lakshya <you@example.com>
2024-12-24 15:45:25 +0530

    game animation logic

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 // animation and box click logic
 var blocks  = document.querySelectorAll(".inner-column");
-for(let i = 0 ; i<blocks.length  ; i++){
-     blocks[i].addEventListener("click",()=>{
-          blocks[i].classList.add("clicked");
-          setTimeout(()=>{
-               blocks[i].classList.remove("clicked");
-          },150)
-     })
+
+function playSound(){
+     var audio = new Audio("./public/assets/green.mp3")
+     audio.play();
 }
 
-//game-logic
+blocks.forEach(block=>
+     block.addEventListener("click",()=>{
+          playSound();
+          block.classList.add("clicked");         
+          console.log(this);
+          
+          setTimeout(()=>{
+               block.classList.remove("clicked");
+          },170)
+     })
+)
 
+var resetBtn = document.querySelector(".reset-btn");
+resetBtn.addEventListener("click",()=>{
+     console.log("game reset")
+})
+
+//game-logic
+const winningPattern = [
+     [1,2,3],
+     [4,5,6],
+     [7,8,9],
+     [1,4,7],
+     [2,5,8],
+     [3,6,9],
+     [1,5,9],
+     [3,5,7]
+]
+
+function checkAnswer(){
+
+}
 
 
